bd9ff5209b0e864073470722572453a18986fa44
‪Tomer Schwartz‬‏ <65711940+kap70tomer@users.noreply.github.com>
2022-01-12 19:12:13 +0200

    passsword to DB root changed
    new pass is root

--- back/dao/connection-wrapper.js
+++ back/dao/connection-wrapper.js
@@ -3,7 +3,7 @@ const sql = require("mysql2");
 const connection = sql.createConnection({
     host: "localhost",
     user: "root",
-    password: "05455",
+    password: "root",
     database: "shopping",
 });
 
